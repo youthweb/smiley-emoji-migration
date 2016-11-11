e0f9fe2055af03c20190a2e543972b5a14f3d19a
Art4 <art4@wlabs.de>
2016-11-11 12:50:30 +0100

    Mehr tests

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -13,6 +13,36 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 	{
 		$parser = new Parser;
 
-		$this->assertTrue(is_array($parser->parseReadme()));
+		$entries = $parser->parseReadme();
+
+		$this->assertTrue(is_array($entries));
+		$this->assertCount(363, $entries);
+	}
+
+	/**
+	 * @dataProvider EntryProvider
+	 */
+	public function testEntryAttributes($entry)
+	{
+		$this->assertTrue(is_array($entry));
+	}
+
+	/**
+	 * EntryProvider
+	 */
+	public function EntryProvider()
+	{
+		$parser = new Parser;
+
+		$return = [];
+
+		$entries = $parser->parseReadme();
+
+		foreach ($entries as $entry)
+		{
+			$return[] = [$entry];
+		}
+
+		return $return;
 	}
 }
