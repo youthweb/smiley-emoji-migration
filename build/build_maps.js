5231ba50140d0157a1c0e00f5008dd66bd02912c
Art4 <art4@wlabs.de>
2018-08-22 16:54:08 +0200

    normalize data from README.md

--- build/build_maps.js
+++ build/build_maps.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
-const Normalizer = require('../lib/emojidata-normalizer');
+const EmojidataNormalizer = require('../lib/emojidata-normalizer');
+const ReadmeNormalizer = require('../lib/readme-normalizer');
 const emojiData = require('emoji-datasource-twitter/emoji.json');
 
-var normalizer = new Normalizer();
-var data = normalizer.normalizeData(emojiData);
+var emojiNormalizer = new EmojidataNormalizer();
+var data = emojiNormalizer.normalizeData(emojiData);
 
 fs.writeFileSync('./dist/emojidata_pretty.json', JSON.stringify(data, null, 4));
+
+var readmeData = fs.readFileSync('./README.md');
+var readmeNormalizer = new ReadmeNormalizer();
+var data = readmeNormalizer.normalizeData(readmeData.toString());
+
+fs.writeFileSync('./dist/readmedata_pretty.json', JSON.stringify(data, null, 4));
 
 console.log('All maps were created.');
