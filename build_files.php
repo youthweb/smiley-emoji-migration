885d7f286e2d1f02d8545bbbfcdb1a3f7f7ff22c
Art4 <art4@wlabs.de>
2018-08-23 15:33:34 +0200

    Delete unused file and PHP code

--- build_files.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('vendor/autoload.php');
-
-$converter = new Youthweb\SmileyEmojiMigration\Converter();
-
-$converter->toJsonFile('dist/smilies_pretty.json', JSON_PRETTY_PRINT);
-
-echo "All files created. 🚀\n";
