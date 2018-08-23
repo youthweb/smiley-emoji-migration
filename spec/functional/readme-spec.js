b7eee8b42e63d05421acd4fa029c04ea3c74ea2a
Art4 <art4@wlabs.de>
2018-08-23 13:36:55 +0200

    Make tests more decriptive

--- spec/functional/readme-spec.js
+++ spec/functional/readme-spec.js
@@ -1,40 +1,47 @@
 const fs = require('fs');
 const Normalizer = require('../../lib/readme-normalizer');
 
+describe('The README.md file is normalized', () => {
+    var dataPromise = new Promise((resolve, reject) => {
+        var readmeData = fs.readFileSync('./README.md');
+        var readmeNormalizer = new Normalizer();
+        var normalized = readmeNormalizer.normalizeData(readmeData.toString());
+
+        resolve(normalized);
+    });
+
+    it('and contains 363 entries', () => {
+        return dataPromise.then((result) => {
+            expect(Object.entries(result).length).toBe(363);
+        })
+    });
+});
 
 describe('The README.md file is normalized', () => {
-    var dataPromise = null;
+    var readmeData = fs.readFileSync('./README.md');
+    var readmeNormalizer = new Normalizer();
+    var normalized = readmeNormalizer.normalizeData(readmeData.toString());
 
-    beforeEach(() => {
-        dataPromise = new Promise((resolve, reject) => {
-            var readmeData = fs.readFileSync('./README.md');
-            var readmeNormalizer = new Normalizer();
-            var normalized = readmeNormalizer.normalizeData(readmeData.toString());
+    for (var shortcode in normalized) {
+        describe('and the entry "'+shortcode+'"', () => {
+            var entry = normalized[shortcode];
 
-            resolve(normalized);
-        });
+            it('has the right properties', () => {
+                expect(Object.keys(entry)).toEqual(['unicode_images', 'smiley_image']);
+            });
 
-    });
-
-    describe('and it contains', () => {
-        it('363 entries', () => {
-            return dataPromise.then((result) => {
-                expect(Object.entries(result).length).toBe(363);
-            })
-        });
-    });
-
-    it('and the entry has a "smiley_image" property', () => {
-        return dataPromise.then((result) => {
-            var url_regex = /^\/dist\/png\-64\/[a-z0-9-]{4,}\.png$/is;
-            for (var shortcode in result) {
-                var entry = result[shortcode];
-
-                expect(entry.hasOwnProperty('smiley_image')).toBe(true);
-                expect(typeof entry.smiley_image).toBe('string');
-                expect(entry.hasOwnProperty('unicode_images')).toBe(true);
+            it('has a correct "unicode_images" property of type string', () => {
                 expect(typeof entry.unicode_images).toBe('object');
-            }
+
+                for (var image of entry.unicode_images) {
+                    expect(typeof image).toBe('string');
+                    expect(image).toMatch(/^[a-z0-9-]{4,}\.png$/is);
+                }
+            });
+
+            it('has a correct "smiley_image" property of type string', () => {
+                expect(typeof entry.smiley_image).toBe('string');
+            });
         });
-    });
+    }
 });
