37d2bb661b4b5f5dab9b756585d0b69d8fbb907c
Art4 <art4@wlabs.de>
2018-08-24 15:16:35 +0200

    Fix maps export

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const emojisData = require('./dist/emojidata_pretty.json');
 const smiliesData = require('./dist/smilies_pretty.json');
 
-module.exports.maps.shortcodeToImagesMap = Object.assign({}, emojisData, smiliesData);
+module.exports.maps = {
+    shortcodeToImagesMap: Object.assign({}, emojisData, smiliesData),
+};
