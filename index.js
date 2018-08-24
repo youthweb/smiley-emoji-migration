91adc9a586c89d464defe43f9d2315f75f358572
Art4 <art4@wlabs.de>
2018-08-24 13:39:17 +0200

    Fix path to maps

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const emojisData = require('dist/emojidata_pretty.json');
-const smiliesData = require('dist/smilies_pretty.json');
+const emojisData = require('./dist/emojidata_pretty.json');
+const smiliesData = require('./dist/smilies_pretty.json');
 
 module.exports.maps.shortcodeToImagesMap = Object.assign({}, emojisData, smiliesData);
